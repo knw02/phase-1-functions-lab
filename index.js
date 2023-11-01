f425d148941229d09c627d627aae79bd2768f3ee
Kalie <kwatsonnov2002@gmail.com>
2023-11-01 18:50:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hqLocation = 42;
+    return Math.abs(location - hqLocation);
+  }
+  function distanceFromHqInFeet(location) {
+    const hqLocation = 42;
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(location - hqLocation);
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+    return distanceInFeet;
+  }
+  function distanceTravelledInFeet(start, end) {
+    const feetPerBlock = 264; // Assuming the conversion rate is 264 feet per block
+    const distanceInBlocks = Math.abs(end - start);
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+    return distanceInFeet;
+  }
+  function calculatesFarePrice(start, destination) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = distanceInBlocks * feetPerBlock;
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      const farePrice = (distanceInFeet - 400) * 0.02;
+      console.log(farePrice); // Add this line for debugging
+      return parseFloat(farePrice.toFixed(2)); // Convert to number
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
